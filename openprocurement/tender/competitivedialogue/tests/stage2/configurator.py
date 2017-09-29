e00b31c04fbd19a821263fd1d1238a6729145405
MotsoVitalii <vitalii.motso@gmail.com>
2017-09-29 15:54:38 +0300

    improve basic test of configurator

--- openprocurement/tender/competitivedialogue/tests/stage2/configurator.py
+++ openprocurement/tender/competitivedialogue/tests/stage2/configurator.py
@@ -1,26 +1,36 @@
 import unittest
+
+from openprocurement.api.tests.base import snitch
+from openprocurement.tender.core.tests.configurator import ConfiguratorTestMixin
+
 from openprocurement.tender.competitivedialogue.adapters import (
     TenderCDEUConfigurator, TenderCDUAConfigurator,
-    TenderCDEUStage2Configurator,TenderCDUAStage2Configurator)
+    TenderCDEUStage2Configurator, TenderCDUAStage2Configurator)
 
 
-class ConfiguratorValueTest(unittest.TestCase):
-    def test_reverse_awarding_criteria(self):
-        self.assertEqual(TenderCDEUConfigurator.reverse_awarding_criteria, False)
-        self.assertEqual(TenderCDUAConfigurator.reverse_awarding_criteria, False)
-        self.assertEqual(TenderCDEUStage2Configurator.reverse_awarding_criteria, False)
-        self.assertEqual(TenderCDUAStage2Configurator.reverse_awarding_criteria, False)
 
-    def test_awarding_criteria_key(self):
-        self.assertEqual(TenderCDEUConfigurator.awarding_criteria_key, 'amountPerfomance')
-        self.assertEqual(TenderCDUAConfigurator.awarding_criteria_key, 'amountPerfomance')
-        self.assertEqual(TenderCDEUStage2Configurator.awarding_criteria_key, 'amountPerfomance')
-        self.assertEqual(TenderCDUAStage2Configurator.awarding_criteria_key, 'amountPerfomance')
+class ConfiguratorTestTenderCDEUStage2Configurator(unittest.TestCase, ConfiguratorTestMixin):
+    configurator_class = TenderCDEUStage2Configurator
+
+
+class ConfiguratorTestTenderCDEUConfigurator(unittest.TestCase, ConfiguratorTestMixin):
+    configurator_class = TenderCDEUConfigurator
+
+
+class ConfiguratorTestTenderCDUAConfigurator(unittest.TestCase, ConfiguratorTestMixin):
+    configurator_class = TenderCDUAConfigurator
+
+
+class ConfiguratorTestTenderCDUAStage2Configurator(unittest.TestCase, ConfiguratorTestMixin):
+    configurator_class = TenderCDUAStage2Configurator
 
 
 def suite():
     current_suite = unittest.TestSuite()
-    current_suite.addTest(unittest.makeSuite(ConfiguratorValueTest))
+    current_suite.addTest(unittest.makeSuite(ConfiguratorTestTenderCDEUStage2Configurator))
+    current_suite.addTest(unittest.makeSuite(ConfiguratorTestTenderCDEUConfigurator))
+    current_suite.addTest(unittest.makeSuite(ConfiguratorTestTenderCDUAConfigurator))
+    current_suite.addTest(unittest.makeSuite(ConfiguratorTestTenderCDUAStage2Configurator))
     return suite
 
 
